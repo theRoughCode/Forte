4b5d1d0bae89016d8c1478cb322fd60c1b1288a3
Chelsea Tanchip <chelseanicolene1030@gmail.com>
2017-10-22 11:51:35 -0400

    Update volume.py

--- src/audio/volume.py
+++ src/audio/volume.py
@@ -7,7 +7,6 @@ Objective:
 """
 
 # Importing libraries
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import math
@@ -56,13 +55,16 @@ class Volume():
         The noise's new level
                 
         """
+        import decimal
         dbs = []
-        for i in range(len(self.dataset.DBA.tolist())):
-            num = float(self.dataset['DBA'][i])
-            volume = (pow(10, (self.dba_to_db(num)/10 - 12)) - (pow(10, -9))) / pow(10, -3)
-            print(volume)
+        data = self.dataset.DBA.tolist()
+        syndata = data[:]
+        for i in range(len(syndata)):
+            num = float(syndata[i])
+            volume = (decimal.Decimal(pow(10, decimal.Decimal(self.dba_to_db(
+                    num)/10 - 12))) - decimal.Decimal(pow(10, -9))) / decimal.Decimal(
+                    pow(10, -3))
             dbs.append(volume)
-                
         return dbs
     
     
@@ -78,30 +80,24 @@ class Volume():
         """
         Reverting dba to db. 
         """
-        return math.pow(10, ((dba - 28)/(33.22)))
+        return pow(10, ((dba - 28)/(33.22)))
     
     def decibel(self, file_name):
         db = 20 * math.log10(self.largest(file_name))
         dba = self.calculate_dba(db)
-        print(db,dba)
-        
         new_column = self.volume_pred(file_name)
         self.dataset['DBChange'] = new_column
         X = self.dataset.iloc[:, 1:2].values
         y = self.dataset.iloc[:, 3].values
-        # Splitting the self.dataset into the Training set and Test set
-        from sklearn.cross_validation import train_test_split
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
         
         # Using random forest regression model to predict new decibel
         from sklearn.ensemble import RandomForestRegressor
         regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
         regressor.fit(X, y)
 
-        y_pred = regressor.predict(dba)
+        y_pred = regressor.predict(self.dba_to_db(dba))
         
-        return (math.pow(10, self.dba_to_db(y_pred)/10 - 12) - (
-                math.pow(10, -9))) / math.pow(10, -3)
+        return math.fabs(y_pred)
     
     
     
